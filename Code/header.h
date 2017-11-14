c818f2e4da16845480579eec6516483c033a246b
Sahil Rajput <sahil.rajput.0196@gmail.com>
2017-11-14 23:28:55 +0530

    V 1.3

--- Code/header.h
+++ Code/header.h
@@ -6,3 +6,12 @@ int user_count = 0;
 int computer_count = 0;
 int randomNo();
 void WhoWouldWin(char);
+std::ostream& bold_on(std::ostream& os)
+{
+    return os << "\e[1m";
+}
+
+std::ostream& bold_off(std::ostream& os)
+{
+    return os << "\e[0m";
+}
--- Code/main.cpp
+++ Code/main.cpp
@@ -28,8 +28,9 @@ void WhoWouldWin(char choice)
     //If both are equal
     if(com == choice)
     {
-        cout <<"\nYou both tied "<<"\t\t\t\tYou : "<<user_count<<" | "<<"Computer : "<<computer_count;
-        cout<<"\nYou : "<<choice<<"\tComputer : "<<com<<"\n";
+        cout <<bold_on<<"\n\t\t\tYou both tied \n"<<bold_off;
+        cout<<bold_on<<"\tYou : "<<bold_off<<user_count<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<computer_count;
+        cout<<bold_on<<"\n\tYou : "<<bold_off<<choice<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<com<<"\n";
     }
     else if (choice == 'R')
     {
@@ -37,15 +38,17 @@ void WhoWouldWin(char choice)
         if (com == 'P')
         {
             ++computer_count;
-            cout<<"\nYou lose "<<"\t\t\t\tYou : "<<user_count<<" | "<<"Computer : "<<computer_count;
-            cout<<"\nYou : "<<choice<<"\tComputer : "<<com<<"\n";
+            cout<<bold_on<<"\n\t\t\tYou lose \n"<<bold_off;
+            cout<<bold_on<<"\tYou : "<<bold_off<<user_count<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<computer_count;
+            cout<<bold_on<<"\n\tYou : "<<bold_off<<choice<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<com<<"\n";
         }
         //Rock beats Scissor
         else
         {
             ++user_count;
-            cout<<"\nYou win "<<"\t\t\t\tYou : "<<user_count<<" | "<<"Computer : "<<computer_count;
-            cout<<"\nYou : "<<choice<<"\tComputer : "<<com<<"\n";
+            cout<<bold_on<<"\n\t\t\tYou win \n"<<bold_off;
+            cout<<bold_on<<"\tYou : "<<bold_off<<user_count<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<computer_count;
+            cout<<bold_on<<"\n\tYou : "<<bold_off<<choice<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<com<<"\n";
         }
     }
     else if (choice == 'S')
@@ -54,15 +57,17 @@ void WhoWouldWin(char choice)
         if (com == 'R')
         {
             ++computer_count;
-            cout<<"\nYou lose "<<"\t\t\t\tYou : "<<user_count<<" | "<<"Computer : "<<computer_count;
-            cout<<"\nYou : "<<choice<<"\tComputer : "<<com<<"\n";
+            cout<<bold_on<<"\n\t\t\tYou lose \n"<<bold_off;
+            cout<<bold_on<<"\tYou : "<<bold_off<<user_count<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<computer_count;
+            cout<<bold_on<<"\n\tYou : "<<bold_off<<choice<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<com<<"\n";
         }
         //Scissor beats Paper
         else
         {
             ++user_count;
-            cout<<"\nYou win "<<"\t\t\t\tYou : "<<user_count<<" | "<<"Computer : "<<computer_count;
-            cout<<"\nYou : "<<choice<<"\tComputer : "<<com<<"\n";
+            cout<<bold_on<<"\n\t\t\tYou win \n"<<bold_off;
+            cout<<bold_on<<"\tYou : "<<bold_off<<user_count<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<computer_count;
+            cout<<bold_on<<"\n\tYou : "<<bold_off<<choice<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<com<<"\n";
         }
     }
     else if (choice == 'P')
@@ -71,21 +76,23 @@ void WhoWouldWin(char choice)
         if (com == 'S')
         {
             ++computer_count;
-            cout<<"\nYou lose "<<"\t\t\t\tYou : "<<user_count<<" | "<<"Computer : "<<computer_count;
-            cout<<"\nYou : "<<choice<<"\tComputer : "<<com<<"\n";
+            cout<<bold_on<<"\n\t\t\tYou lose \n"<<bold_off;
+            cout<<bold_on<<"\tYou : "<<bold_off<<user_count<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<computer_count;
+            cout<<bold_on<<"\n\tYou : "<<bold_off<<choice<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<com<<"\n";
         }
         //Paper beats Rock
         else
         {
             ++user_count;
-            cout<<"\nYou win "<<"\t\t\t\tYou : "<<user_count<<" | "<<"Computer : "<<computer_count;
-            cout<<"\nYou : "<<choice<<"\tComputer : "<<com<<"\n";
+            cout<<bold_on<<"\n\t\t\tYou win \n"<<bold_off;
+            cout<<bold_on<<"\tYou : "<<bold_off<<user_count<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<computer_count;
+            cout<<bold_on<<"\n\tYou : "<<bold_off<<choice<<"\t\t   | \t\t"<<bold_on<<"Computer : "<<bold_off<<com<<"\n";
         }
     }
     //Invalid Choice
-    else
+    else if(choice!='E' || choice == 'e')
     {
-        cout<<"\nI'm [S]MART, Enter right choice [ R , P , S ]\n";
+        cout<<bold_on<<"\n\t\t\tI'm [S]MART, Enter right choice [ R , P , S ]\n"<<bold_off;
     }
 
 }
@@ -96,11 +103,11 @@ int main()
     char request = 'Z';
     while(request != 'E')
     {
-        cout<<"\t\t[R]ock\t[P]aper\t[S]cissor\n";
+        cout<<"\t\t"<<bold_on<<"[R]"<<bold_off<<"ock\t"<<bold_on<<"[P]"<<bold_off<<"aper\t"<<bold_on<<"[S]"<<bold_off<<"cissor";
+        cout<<"\t"<<bold_on<<"[E]"<<bold_off<<"xit\n";
         cin>>choice;
         choice = toupper(choice);
         WhoWouldWin(choice);
-        cout<<"\n\t\t\t\t\tPress [E] for Exit\n";
         request = toupper(choice);
     }
     return 0;
